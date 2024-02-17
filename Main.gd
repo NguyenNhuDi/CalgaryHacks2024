1779b52653b6cce3142d16e8aa344c9c859fed3b
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2024-02-17 13:51:20 -0700

    b2 person test

--- Main.gd
+++ Main.gd
@@ -1,10 +1,12 @@
 extends Node2D
+var Person_Obj = preload("res://Person_Obj.gd")
 
 
-
-func _on_b_2_is_handsome_pressed():
-	print("b2 is cute :3")
-
-
-func _on_hello_world_pressed():
-	print("Hello World")
+func _on_button_pressed():
+	var p1 = Person_Obj.new("B2", 21, 2000, 5)
+	
+	print("Bartu attributes")
+	print(p1.age)
+	print(p1.income)
+	print(p1.happiness)
+	print(p1.pName)
---
+++ Person_Obj.gd
@@ -0,0 +1,15 @@
+extends Node
+
+class_name Person
+
+var pName: String
+var age: int
+var income: int
+var happiness: int
+
+func _init(n:String, a:int, i:int, h:int):
+	pName = n
+	age = a
+	income = i
+	happiness = h
+	
