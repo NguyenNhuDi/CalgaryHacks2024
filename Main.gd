579ba08fc3b907b5d748c142983ef90ef00f21ac
BartuO <bartuokan1@gmail.com>
2024-02-17 15:04:02 -0700

    add createRandomPerson
    creates 2 random people with randomized attributes

--- Main.gd
+++ Main.gd
@@ -1,12 +1,59 @@
 extends Node2D
 var Person_Obj = preload("res://Person_Obj.gd")
 
+var names = ["Bartu", "Di", "Brett", "Taylor", "Lucy"]
+var ages = [13, 34, 45, 66, 21, 25, 66, 99, 101230, 3, 5, 3434]
+var incomes = [2333, 21, 4544, 5666, 909, 4343, 95959, 20333]
+var happinesses = [0.7, 0.3, 0.4, 0.99, 0.1, 0.12, 0.88, 0.24]
+
+
+
+#Returns a list [p1, p2] where p1 and p2 are person objects
+#GDScript doesnt have tuples so i needed to use list of 2.
+
+func createRandomPerson():
+	var names_copy = names.duplicate()
+	var ages_copy = ages.duplicate()
+	var incomes_copy = incomes.duplicate()
+	var happinesses_copy = happinesses.duplicate()
+	
+	var rand_name1 = names_copy.pop_at(randi_range(0, len(names_copy) - 1))
+	var rand_age1 = ages_copy.pop_at(randi_range(0, len(ages_copy) - 1))
+	var rand_income1 = incomes_copy.pop_at(randi_range(0, len(incomes_copy) - 1))
+	var rand_happiness1 = happinesses_copy.pop_at(randi_range(0, len(happinesses_copy) - 1))
+	
+	var rand_name2 = names_copy.pop_at(randi_range(0, len(names_copy) - 1))
+	var rand_age2 = ages_copy.pop_at(randi_range(0, len(ages_copy) - 1))
+	var rand_income2 = incomes_copy.pop_at(randi_range(0, len(incomes_copy) - 1))
+	var rand_happiness2 = happinesses_copy.pop_at(randi_range(0, len(happinesses_copy) - 1))
+	
+	
+	
+	
+	var p1 = Person_Obj.new(rand_name1, rand_age1, rand_income1, rand_happiness1)  
+	var p2 = Person_Obj.new(rand_name2, rand_age2, rand_income2, rand_happiness2)  
+	
+
+	return [p1, p2]
+
 
 func _on_button_pressed():
-	var p1 = Person_Obj.new("B2", 21, 2000, 5)
 	
-	print("Bartu attributes")
-	print(p1.age)
-	print(p1.income)
-	print(p1.happiness)
-	print(p1.pName)
+	var two_people = createRandomPerson()
+	
+	print("P1 attributes:")
+	print("Name: ", two_people[0].pName)
+	print("Age: ", two_people[0].age)
+	print("Income: ", two_people[0].income)
+	print("Happiness: ", two_people[0].happiness)
+	
+	print("\n\n\n")
+	
+	print("P2 attributes:")
+	print("Name: ", two_people[1].pName)
+	print("Age: ", two_people[1].age)
+	print("Income: ", two_people[1].income)
+	print("Happiness: ", two_people[1].happiness)
+	
+	print("\n\n\n")
+	
