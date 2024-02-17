7304ccc5f32c79e86ddcba4d2eaa84c263af6474
samanthaaliu <samantha.liu10@gmail.com>
2024-02-17 14:46:23 -0700

    Added Happiness and Profit Bars
    bars get values from main.gd

--- Main.gd
+++ Main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 var Person_Obj = preload("res://Person_Obj.gd")
 
+var happiness = 33.33
+var money = 10
 
 func _on_button_pressed():
 	var p1 = Person_Obj.new("B2", 21, 2000, 5)
@@ -10,3 +12,5 @@ func _on_button_pressed():
 	print(p1.income)
 	print(p1.happiness)
 	print(p1.pName)
+
+
