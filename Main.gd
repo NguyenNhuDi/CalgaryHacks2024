74d69a93f8f14fa043e1795d902d91dd6a114db4
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2024-02-17 14:50:39 -0700

    remove person in room added

--- Main.gd
+++ Main.gd
@@ -2,14 +2,38 @@ extends Node2D
 var Person_Obj = preload("res://Person_Obj.gd")
 
 var rooms = {}
+var fPerson = Person_Obj.new("null", -1, -1, 0)
+
 
 func store_person_in_room(person:Person, room:String):
+	assert(room in rooms, "%s not in rooms" % [room])
+	
 	rooms[room] = person
+	person.rName = room
+
+func remove_person_in_room(room:String):
+	assert(room in rooms, "%s not in rooms" % [room])
+	rooms[room] = fPerson
 
 func _ready():
-	var fPerson = Person_Obj.new("null", -1, -1, 0)
 	# initialize rooms:
 	for i in range(6):
 		var rName = "room_%d" % [i]
 		rooms[rName] = fPerson
+		
+	var b2 = Person_Obj.new("Bartu", 21, 2000, 0.001)
+	
+	store_person_in_room(b2, "room_2")
+	
+	print("Person in room_2:")
+	print(rooms["room_2"].pName)
+	
+	print("removing person...")
+	remove_person_in_room("room_2")
+	
+	print("Person in room_2:")
+	print(rooms["room_2"].pName)
+	
+	remove_person_in_room("room_10")
+	
 		
--- Person_Obj.gd
+++ Person_Obj.gd
@@ -6,10 +6,12 @@ var pName: String
 var age: int
 var income: int
 var happiness: float
+var rName: String
 
 func _init(n:String, a:int, i:int, h:float):
 	pName = n
 	age = a
 	income = i
 	happiness = h
+	rName = "null"
 	
